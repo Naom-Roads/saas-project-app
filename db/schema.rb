672d0169c8fc5ac694f701d17120b5053c503045
Naom-Roads <nrrodrig@gmail.com>
2022-01-19 14:56:56 +0100

    Added tickets scaffold and updated tickets migrate with a reference to users with foreign key, also removed extra scaffold, and users field in ticket creation form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_131050) do
+ActiveRecord::Schema.define(version: 2022_01_19_134846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_01_19_131050) do
     t.string "ticket_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,9 +52,11 @@ ActiveRecord::Schema.define(version: 2022_01_19_131050) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "tickets", "users"
 end
