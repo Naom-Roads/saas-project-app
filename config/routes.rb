c5a46d14faf84a86b71d77db27438d6667c44b00
Naom-Roads <nrrodrig@gmail.com>
2022-01-25 13:30:02 +0100

    Added default route for url options in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+ 
+  default_url_options :host => "localhost:3000"
+
   resources :tickets
   devise_for :users, path: 'auth', controllers: { sign_in: 'login',
     sign_out: 'logout',
