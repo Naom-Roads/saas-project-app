9845e9bf6416765842b5812d5054a15ffbe7bb2d
Naom-Roads <nrrodrig@gmail.com>
2022-01-25 13:28:01 +0100

    Modified routes to test production behavior

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :tickets
-  devise_for :users, controllers: { sessions: "users/sessions"}
+  devise_for :users, path: 'auth', controllers: { sign_in: 'login',
+    sign_out: 'logout',
+      confirmations: 'confirmations'
+    }
 
   root 'home#index'
   
