d74f1fc90775b947899f28af643ab5252e714d05
Naom-Roads <nrrodrig@gmail.com>
2022-01-25 13:16:48 +0100

    changed default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
  
-  config.action_mailer.default_url_options = { :host => 'naoms-saas-project.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'https://naoms-saas-project.herokuapp.com', :protocol => 'https'}
   config.action_mailer.delivery_method = :smtp
 end
