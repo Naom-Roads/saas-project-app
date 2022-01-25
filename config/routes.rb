50feabea1d31d2c3ab72b35da17c9f1c64063601
Naom-Roads <nrrodrig@gmail.com>
2022-01-25 13:34:39 +0100

    Changed routes back to original settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
- 
-  default_url_options :host => "localhost:3000"
-
   resources :tickets
-  devise_for :users, path: 'auth', controllers: { sign_in: 'login',
-    sign_out: 'logout',
-      confirmations: 'confirmations'
-    }
+  devise_for :users, controllers: { sessions: "users/sessions"}
 
   root 'home#index'
   
