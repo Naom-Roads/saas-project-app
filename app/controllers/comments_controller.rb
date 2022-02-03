80ed2f695892275029976e0b14e008ae699df1b0
Naom-Roads <nrrodrig@gmail.com>
2022-02-03 15:22:59 +0100

    Added note by routes for comment, added create, show, and destroy in comment controller, created view for comment and added partial to ticket show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   default_url_options :host => 'localhost:3000'
 
-
   resources :tickets
 
-
   devise_for :users
+
   devise_scope :user do
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
@@ -15,5 +14,7 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'dashboard#show', as: 'user'
   get 'users/:id', to: 'dashboard#index'
 
+  get 'comments', to: 'comments#show' #should this be show or index? Intended behavior is that they reply on a ticket comment thread
+  post 'comment', to: 'comments#create'
 
 end
