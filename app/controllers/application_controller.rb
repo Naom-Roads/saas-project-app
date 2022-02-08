12f649630d36131968905b2e75f27279a19521ce
Naom-Roads <nrrodrig@gmail.com>
2022-02-08 14:41:35 +0100

    Added question and note on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token #need to discuss another way to fix invalid auth token
-  before_action :authenticate_user!
+  before_action :authenticate_user! #do I need to add this on other pages if it is here?
 
 
 end
