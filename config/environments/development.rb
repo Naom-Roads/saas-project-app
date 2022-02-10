414c34f27b1d5c9eaca73b0420a5ef249fd801df
Naom-Roads <nrrodrig@gmail.com>
2022-02-10 16:25:26 +0100

    Added config for perform deliveries in development rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
@@ -37,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
