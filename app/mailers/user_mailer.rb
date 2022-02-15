35f8022fbcdef751ea5e29a8169270b5b119070d
Naom-Roads <nrrodrig@gmail.com>
2022-02-15 14:47:41 +0100

    Made a change to url so that their is a link that redirects to ticket in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
   def ticket_comment_email(user, ticket)
     @user = user
     @ticket = ticket
-    @url =  'localhost:3000/tickets/:ticket_id'
+    @url =  ticket_url(@ticket)
     mail(to: @user.email, subject: "A Comment was added to ticket #{@ticket.ticket_number}")
   end
-
 
 end
